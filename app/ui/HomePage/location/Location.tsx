c7f50a7b1fb5675dae6fcb50dfd1ff007865fed6
Dmytro Petyshyn <dev6012@gmail.com>
2024-01-17 01:13:01 +0200

    style: implement responsive slider on Locations section

--- app/ui/HomePage/location/Location.tsx
+++ app/ui/HomePage/location/Location.tsx
@@ -1,120 +1,37 @@
 'use client'
 
 import { ILocation } from '@/app/lib/interfaces'
-import { Box, Flex, Heading, IconButton, Text } from '@chakra-ui/react'
-import Image from 'next/image'
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
-import 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/free-mode'
-import 'swiper/css/navigation'
-import 'swiper/css/thumbs'
+import { Flex, Heading } from '@chakra-ui/react'
 
 import SectionWrapper from '../../sectionWrapper/SectionWrapper'
-import Arrow from '../../svg/Arrow'
-import ArrowLeftIcon from '../../svg/ArrowLeftIcon'
-
-const SwiperNavigation = ({
-	order,
-	length,
-	isLastSlide,
-}: {
-	order: string
-	length: string
-	isLastSlide: boolean
-}) => {
-	const swiper = useSwiper()
-
-	return (
-		<Flex align={'center'}>
-			<IconButton
-				color={parseInt(order) === 1 ? 'unfocus' : 'currentColor'}
-				aria-label="previous slide"
-				variant={'ghost'}
-				_hover={{ bg: 'transparent' }}
-				icon={<ArrowLeftIcon />}
-				onClick={() => swiper.slidePrev()}
-			/>
-			<Text fontSize={'20px'} fontStyle={'normal'} fontWeight={600} lineHeight={1}>
-				{`${order} / ${length}`}
-			</Text>
-			<IconButton
-				color={isLastSlide ? 'unfocus' : 'currentColor'}
-				aria-label="previous slide"
-				variant={'ghost'}
-				_hover={{ bg: 'transparent' }}
-				onClick={() => swiper.slideNext()}
-				icon={<Arrow />}
-			/>
-		</Flex>
-	)
-}
+import LocationsSlider from './locationsSlider/LocationsSlider'
 
 const Location = ({
-	locations,
 	title,
 	subTitle,
+	locations,
 }: {
-	locations: ILocation[]
 	title: string
 	subTitle: string
+	locations: ILocation[]
 }) => {
 	return (
 		<SectionWrapper bg={'accent'}>
-			<Flex>
-				<Heading as={'h3'}>{title}</Heading>
-				<Heading as={'h2'}>{subTitle}</Heading>
+			<Flex mb={'60px'} flexDir={{ base: 'column', lg: 'row' }} gap={{ base: '30px', lg: '0px' }}>
+				<Heading flex={2} fontSize={'20px'} fontWeight={600} lineHeight={1} as={'h3'}>
+					{title}
+				</Heading>
+				<Heading
+					flex={2.15}
+					as={'h2'}
+					fontSize={{ base: '24px', lg: '40px' }}
+					fontWeight={500}
+					lineHeight={1}
+				>
+					{subTitle}
+				</Heading>
 			</Flex>
-			<Swiper
-				className="locationSlider"
-				navigation={false}
-				slidesPerView={1}
-				breakpoints={{
-					320: {
-						slidesPerView: 1,
-						slidesPerGroup: 1,
-					},
-				}}
-			>
-				{locations.length > 0 &&
-					locations.map((location, idx) => {
-						const { text, img } = location
-
-						return (
-							<SwiperSlide key={idx}>
-								<Box
-									display="flex"
-									gap="40px"
-									borderBottom="2px solid #0e0d0d"
-									w="100%"
-									flexDirection={{ base: 'column', md: 'row' }}
-									alignItems="center"
-								>
-									<Box position="relative" w={200} height={200} flex={2}>
-										<Image
-											src={img || '/img/productPlaceholder.jpg'}
-											alt={'location photo'}
-											fill
-											style={{
-												objectFit: 'cover',
-											}}
-										/>
-									</Box>
-									<Flex flex={2} flexDir={'column'} justify={'center'}>
-										<Text>{text}</Text>
-										<Flex gap="8px" mt="8px">
-											<SwiperNavigation
-												order={(idx + 1).toString()}
-												length={locations.length.toString()}
-												isLastSlide={idx === locations.length - 1}
-											/>
-										</Flex>
-									</Flex>
-								</Box>
-							</SwiperSlide>
-						)
-					})}
-			</Swiper>
+			<LocationsSlider locations={locations} />
 		</SectionWrapper>
 	)
 }
