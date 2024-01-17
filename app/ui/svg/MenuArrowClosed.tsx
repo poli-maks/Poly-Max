a2212fedd2b3a33af08b111537d158117dadf5b5
LosOleksandr <losalexander2003@gmail.com>
2024-01-17 17:32:08 +0200

    feat: implemnet searchParams logic on catalog page

---
+++ app/ui/svg/MenuArrowClosed.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MenuArrowClosed = () => {
+	return (
+		<svg width="16" height="16" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M1.5 11L6.5 6L1.5 1"
+				stroke="#212121"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	)
+}
+
+export default MenuArrowClosed
