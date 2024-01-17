68a5d3abc2bb9df97237a664973ceab382845253
GoodPointt <dev6012@gmail.com>
2024-01-17 17:05:48 +0200

    feat: implement notFound and successSubmit pages

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -6,13 +6,26 @@ import SectionWrapper from '../ui/sectionWrapper/SectionWrapper'
 const NotFound = () => {
 	return (
 		<SectionWrapper>
-			<Center flexDir={'column'} gap={6} h={480}>
-				<Heading as={'h2'} fontWeight={'bold'}>
-					😒 OOPS!
+			<Center flexDir={'column'} h={480}>
+				<Text fontSize={'100px'} fontWeight={600} mb={'50px'}>
+					404
+				</Text>
+				<Heading as={'h2'} fontSize={'40px'} fontWeight={'500'} mb={'30px'} textAlign={'center'}>
+					Diese Seite fehlt
 				</Heading>
-				<Text>Looks like something went wrong</Text>
+				<Text
+					textAlign={'center'}
+					fontSize={'18px'}
+					fontWeight={400}
+					line-height={1.4}
+					maxW={'510px'}
+					mb={'40px'}
+				>
+					Keine Sorge, es passiert jedem, lass uns sehen, ob wir Sie in die richtige Richtung lenken
+					können.
+				</Text>
 
-				<Button as={Link} href="/" variant={'accent'}>
+				<Button as={Link} href="/" variant={'accentAlt'} maxW={'340px'}>
 					Let&apos;s try again
 				</Button>
 			</Center>
