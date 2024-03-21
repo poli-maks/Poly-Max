d193feefedfe459b3adbc027b2e5ce0a395136f4
NikKms <mykola.kamyshnikov@gmail.com>
2024-03-21 13:33:08 +0200

    fix: midlware sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,89 +2,109 @@ import { fetchAllProducts } from './lib/api/services'
 import { IProduct } from './lib/interfaces'
 
 async function sitemap() {
-	const products: IProduct[] = []
+	const data = await fetchAllProducts('de', 1)
 
-	const fetchData = async (language: string) => {
-		const data = await fetchAllProducts(language, 1)
-		if (typeof data === 'object') {
-			const { count } = data
-			const pageCount = Math.ceil(count / 25)
-			const productEntries = []
-			for (let i = 1; i <= pageCount; i += 1) {
-				const res = await fetchAllProducts(language, i, 25)
+	let products: IProduct[] = []
+
+	if (typeof data === 'object') {
+		const { count } = data
+
+		const pageCount = Math.ceil(count / 25)
+
+		for (let i = 1; i <= pageCount; i += 1) {
+			const res = await fetchAllProducts('de', i, 25)
+
+			{
 				if (typeof res === 'object') {
 					const { data } = res
-					products.push(...data)
+					products = [...products, ...data]
 				}
 			}
-			productEntries.push(
-				...products.map(({ attributes }) => ({
-					url: `${process.env.NEXT_PUBLIC_URL}/${language}/catalog/${attributes.uid}`,
-					lastModified: new Date(attributes.updatedAt),
-					changeFrequency: 'monthly',
-					priority: 1,
-				}))
-			)
-
-			return productEntries
 		}
-
-		return []
 	}
 
-	const [productEntriesDe, productEntriesEn] = await Promise.all([fetchData('de'), fetchData('en')])
+	if (typeof data === 'object') {
+		const { count } = data
+
+		const pageCount = Math.ceil(count / 25)
+
+		for (let i = 1; i <= pageCount; i += 1) {
+			const res = await fetchAllProducts('en', i, 25)
+
+			{
+				if (typeof res === 'object') {
+					const { data } = res
+					products = [...products, ...data]
+				}
+			}
+		}
+	}
+
+	const productEntriesDe = products.map(({ attributes }) => ({
+		url: `${process.env.NEXT_PUBLIC_URL}/de/catalog/${attributes.uid}`,
+		lastModified: new Date(attributes.updatedAt),
+		changeFrequency: 'monthly',
+		priority: 1,
+	}))
+
+	const productEntriesEn = products.map(({ attributes }) => ({
+		url: `${process.env.NEXT_PUBLIC_URL}/en/catalog/${attributes.uid}`,
+		lastModified: new Date(attributes.updatedAt),
+		changeFrequency: 'monthly',
+		priority: 1,
+	}))
 
 	return [
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/favicon.ico`,
+			url: process.env.NEXT_PUBLIC_URL + '/favicon.ico',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 0.3,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/en`,
+			url: process.env.NEXT_PUBLIC_URL + '/en',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 1,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/en/about`,
+			url: process.env.NEXT_PUBLIC_URL + '/en' + '/about',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.8,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/en/catalog`,
+			url: process.env.NEXT_PUBLIC_URL + '/en' + '/catalog',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
 			priority: 0.5,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/en/contact`,
+			url: process.env.NEXT_PUBLIC_URL + '/en' + '/contact',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.8,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/de`,
+			url: process.env.NEXT_PUBLIC_URL + '/de',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 1,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/de/about`,
+			url: process.env.NEXT_PUBLIC_URL + '/de' + '/about',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.8,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/de/catalog`,
+			url: process.env.NEXT_PUBLIC_URL + '/de' + '/catalog',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
 			priority: 0.5,
 		},
 		{
-			url: `${process.env.NEXT_PUBLIC_URL}/de/contact`,
+			url: process.env.NEXT_PUBLIC_URL + '/de' + '/contact',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.8,
