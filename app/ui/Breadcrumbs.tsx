d4ca17c158fa9821e9a7a5757934308adc8b825b
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-03 20:55:59 +0300

    Update Breadcrumbs.tsx

--- app/ui/Breadcrumbs.tsx
+++ app/ui/Breadcrumbs.tsx
@@ -1,35 +1,33 @@
-'use client'
+import React, { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
-import { usePathname } from 'next/navigation'
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
+const Breadcrumbs = () => {
+    const [isClient, setIsClient] = useState(false);
+    const pathname = usePathname();
 
-export const Breadcrumbs = () => {
-  const [isClient, setIsClient] = useState(false)
-  const pathname = isClient ? usePathname() : ''
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
+    const breadcrumbs = pathname ? pathname.split('/').filter((x) => x) : [];
 
-  const breadcrumbs = pathname ? pathname.split('/').filter((x) => x) : []
+    return (
+        <nav aria-label="Breadcrumb">
+            <ol>
+                {breadcrumbs.map((crumb, index) => {
+                    const href = `/${breadcrumbs.slice(0, index + 1).join('/')}`;
+                    const isLast = index === breadcrumbs.length - 1;
 
-  return (
-    <nav aria-label="breadcrumb">
-      <ol>
-        {breadcrumbs.map((crumb, index) => {
-          const href = `/${breadcrumbs.slice(0, index + 1).join('/')}`
-          const isLast = index === breadcrumbs.length - 1
-          
-          return (
-            <li key={href}>
-              {isLast ? <span>{crumb}</span> : <Link href={href}>{crumb}</Link>}
-            </li>
-          )
-        })}
-      </ol>
-    </nav>
-  )
-}
+                    return (
+                        <li key={href}>
+                            {isLast ? <span>{crumb}</span> : <Link href={href}>{crumb}</Link>}
+                        </li>
+                    );
+                })}
+            </ol>
+        </nav>
+    );
+};
 
-export default Breadcrumbs
+export default Breadcrumbs;
