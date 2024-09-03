e6da8b4643e6394a7133cb61c33f6e2e7ef65b81
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-03 19:56:04 +0300

    Update Breadcrumbs.tsx

--- app/ui/Breadcrumbs.tsx
+++ app/ui/Breadcrumbs.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import React from 'react'
 
 const Breadcrumbs = () => {
-	const router = useRouter();
-	const pathnames = router.pathname.split('/').filter((x) => x);
+  const router = useRouter()
+  const pathnames = router.pathname.split('/').filter((x) => x)
 
-	return (
-		<nav aria-label="breadcrumb">
-			<ul className="breadcrumb">
-				<li>
-					<Link href="/">Home</Link>
-				</li>
-				{pathnames.map((value, index) => {
-					const href = '/' + pathnames.slice(0, index + 1).join('/');
-					const isLast = index === pathnames.length - 1;
+  return (
+    <nav aria-label="breadcrumb">
+      <ul className="breadcrumb">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        {pathnames.map((value, index) => {
+          const href = '/' + pathnames.slice(0, index + 1).join('/')
+          const isLast = index === pathnames.length - 1
 
-					return (
-						<li key={href}>
-							{isLast ? <span>{value}</span> : <Link href={href}>{value}</Link>}
-						</li>
-					);
-				})}
-			</ul>
-		</nav>
-	);
-};
+          return (
+            <li key={href}>
+              {isLast ? <span>{value}</span> : <Link href={href}>{value}</Link>}
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
 
-export default Breadcrumbs;
+export default Breadcrumbs
