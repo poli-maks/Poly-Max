828e7d55bffef9199c223275feef8859b4101e04
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-03 22:42:22 +0300

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,18 +1,16 @@
-// Import necessary components and configurations
-import Footer from '@/app/ui/footer/Footer';
-import Header from '@/app/ui/header/Header';
-import Breadcrumbs from '@/app/ui/Breadcrumbs'; // Import Breadcrumbs component
-import { Locale, i18n } from '@/i18n.config';
-import { Manrope } from 'next/font/google';
+import Footer from '@/app/ui/footer/Footer'
+import Header from '@/app/ui/header/Header'
+import { Locale, i18n } from '@/i18n.config'
+// import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
 
-import '@/app/ui/globals.css';
+import '@/app/ui/globals.css'
 
-import { IParams } from '../lib/interfaces';
-import { Providers } from '../providers';
+import { IParams } from '../lib/interfaces'
+import { Providers } from '../providers'
 
-const manrope = Manrope({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ['latin'] })
 
-// Generate metadata for SEO and other purposes
 export const generateMetadata = async ({ params: { lang } }: IParams) => {
 	return {
 		keywords: process.env?.NEXT_PUBLIC_KEYWORDS?.replaceAll(' ', '').split(','),
@@ -32,36 +30,34 @@ export const generateMetadata = async ({ params: { lang } }: IParams) => {
 		twitter: {
 			card: 'summary_large_image',
 		},
+
 		metadataBase: new URL(process.env.NEXT_PUBLIC_URL as string),
 		openGraph: {
 			images: '/opengraph-image.png',
 		},
-	};
-};
-
-// Generate static parameters for internationalization
-export async function generateStaticParams() {
-	return i18n.locales.map((locale) => ({ lang: locale }));
+	}
 }
 
-// Root layout for the application
+export async function generateStaticParams() {
+	return i18n.locales.map((locale) => ({ lang: locale }))
+}
+
 export default function RootLayout({
 	children,
 	params: { lang },
 }: {
-	children: React.ReactNode;
-	params: { lang: Locale };
+	children: React.ReactNode
+	params: { lang: Locale }
 }) {
 	return (
 		<html lang={lang}>
 			<body className={manrope.className}>
 				<Providers>
 					<Header lang={lang} />
-					<Breadcrumbs /> {/* Add Breadcrumbs component here */}
 					<main>{children}</main>
 					<Footer lang={lang} />
 				</Providers>
 			</body>
 		</html>
-	);
+	)
 }
