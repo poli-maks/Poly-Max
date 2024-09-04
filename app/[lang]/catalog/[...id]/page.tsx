dcfc09c9c45f9c1fc4fc2d2e8746f5f667b74da3
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-05 01:42:26 +0300

    update

--- app/[lang]/catalog/[...id]/page.tsx
+++ app/[lang]/catalog/[...id]/page.tsx
@@ -4,6 +4,9 @@ import { Suspense } from 'react';
 import { IParams } from '@/app/lib/interfaces';
 
 export const generateMetadata = async ({ params: { productName, lang } }: IParams) => {
+  // Ensure productName is defined and of type 'string'
+  if (!productName) return notFound(); // Handle the case where productName is undefined
+
   const product = await getProductByName(lang, productName);
 
   if (!product) return notFound();
@@ -22,6 +25,7 @@ export const generateMetadata = async ({ params: { productName, lang } }: IParam
 };
 
 const ProductPage: React.FC<IParams> = ({ params: { lang, productName } }) => {
+  // Ensure productName is defined and of type 'string'
   if (!productName) return notFound();
 
   return (
