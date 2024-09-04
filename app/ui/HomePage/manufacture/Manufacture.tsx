0d95018ded884ac38540d969dbf9be02a1af6522
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-04 19:55:04 +0300

    update check

--- app/ui/HomePage/manufacture/Manufacture.tsx
+++ app/ui/HomePage/manufacture/Manufacture.tsx
@@ -1,11 +1,11 @@
-import { Locale } from "@/i18n.config";
-import { Box, Heading, Button, Text, Flex } from "@chakra-ui/react";
-import Link from "next/link";
+import { Locale } from '@/i18n.config';
+import { Box, Heading, Button, Text, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
 
-import SectionWrapper from "../../sectionWrapper/SectionWrapper";
-import Arrow from "../../svg/Arrow";
-import { theme } from "../../theme";
-import Swiper from "./Swiper/manufactureSwiper";
+import SectionWrapper from '../../sectionWrapper/SectionWrapper';
+import Arrow from '../../svg/Arrow';
+import { theme } from '../../theme';
+import Swiper from './Swiper/manufactureSwiper';
 
 interface ManufactureProps {
   lang: Locale;
@@ -27,43 +27,43 @@ interface ManufactureProps {
 
 const Manufacture: React.FC<ManufactureProps> = ({ lang, dictionary }) => {
   return (
-    <SectionWrapper style={{ paddingBottom: "0" }}>
-      <Flex flexDirection={{ base: "column", lg: "row" }}>
+    <SectionWrapper style={{ paddingBottom: '0' }}>
+      <Flex flexDirection={{ base: 'column', lg: 'row' }}>
         <Heading
-          as={"h3"}
-          fontSize="20px"
+          as={'h3'}
+          fontSize='20px'
           color={theme.colors.hText}
-          marginBottom={{ base: "30px", lg: "0" }}
+          marginBottom={{ base: '30px', lg: '0' }}
           flex={{ lg: 2 }}
-          style={{ lineHeight: "20px", fontWeight: "600" }}
+          style={{ lineHeight: '20px', fontWeight: '600' }}
         >
           {dictionary.homePage.manufacture.title}
         </Heading>
         <Box flex={{ lg: 2.15 }}>
           <Heading
-            as={"h2"}
-            fontSize={{ base: "24px", lg: "40px" }}
-            lineHeight={{ base: "24px", lg: "40px" }}
+            as={'h2'}
+            fontSize={{ base: '24px', lg: '40px' }}
+            lineHeight={{ base: '24px', lg: '40px' }}
             color={theme.colors.hText}
-            fontWeight="500"
-            marginBottom={{ base: "20px", lg: "40px" }}
+            fontWeight='500'
+            marginBottom={{ base: '20px', lg: '40px' }}
           >
             {dictionary.homePage.manufacture.descriptionUp}
           </Heading>
           <Text
-            fontSize={{ base: "12px", lg: "18px" }}
-            lineHeight={{ base: "16.8px", lg: "25.2px" }}
+            fontSize={{ base: '12px', lg: '18px' }}
+            lineHeight={{ base: '16.8px', lg: '25.2px' }}
             color={theme.colors.bodyText}
-            marginBottom={{ base: "20px", lg: "60px" }}
+            marginBottom={{ base: '20px', lg: '60px' }}
           >
             {dictionary.homePage.manufacture.descriptionUnder}
           </Text>
           <Button
-            variant={"arrow"}
-            fontSize={{ base: "18px", lg: "20px" }}
-            lineHeight="25.2px"
-            marginBottom={{ base: "30px", lg: "60px" }}
-            w={{ base: "100%", lg: "340px" }}
+            variant={'arrow'}
+            fontSize={{ base: '18px', lg: '20px' }}
+            lineHeight='25.2px'
+            marginBottom={{ base: '30px', lg: '60px' }}
+            w={{ base: '100%', lg: '340px' }}
             as={Link}
             href={`/${lang}/about`}
             rightIcon={<Arrow />}
@@ -74,37 +74,37 @@ const Manufacture: React.FC<ManufactureProps> = ({ lang, dictionary }) => {
       </Flex>
       <Swiper />
       <Flex
-        display="flex"
-        flexDirection={{ base: "column", lg: "row" }}
-        justifyContent={"space-between"}
-        marginTop={{ base: "30px", lg: "60px" }}
+        display='flex'
+        flexDirection={{ base: 'column', lg: 'row' }}
+        justifyContent={'space-between'}
+        marginTop={{ base: '30px', lg: '60px' }}
       >
-        <Box flex={{ lg: 2 }} as={"span"}></Box>
+        <Box flex={{ lg: 2 }} as={'span'}></Box>
         <Box flex={{ lg: 2.15 }}>
           <Text
-            fontSize={"20px"}
+            fontSize={'20px'}
             fontWeight={600}
-            marginBottom={{ base: "20px", lg: "30px" }}
+            marginBottom={{ base: '20px', lg: '30px' }}
           >
             {dictionary.homePage.manufacture.aboutPoliMaksTitle}
           </Text>
           <Text
-            fontSize={{ base: "12px", lg: "18px" }}
-            lineHeight={{ base: "16.8px", lg: "25.2px" }}
+            fontSize={{ base: '12px', lg: '18px' }}
+            lineHeight={{ base: '16.8px', lg: '25.2px' }}
           >
             {dictionary.homePage.manufacture.aboutPoliMaks}
           </Text>
           <Text
-            fontSize={"20px"}
+            fontSize={'20px'}
             fontWeight={600}
-            marginBottom={{ base: "20px", lg: "30px" }}
-            marginTop={{ base: "30px", lg: "60px" }}
+            marginBottom={{ base: '20px', lg: '30px' }}
+            marginTop={{ base: '30px', lg: '60px' }}
           >
             {dictionary.homePage.manufacture.deliveryTitle}
           </Text>
           <Text
-            fontSize={{ base: "12px", lg: "18px" }}
-            lineHeight={{ base: "16.8px", lg: "25.2px" }}
+            fontSize={{ base: '12px', lg: '18px' }}
+            lineHeight={{ base: '16.8px', lg: '25.2px' }}
           >
             {dictionary.homePage.manufacture.delivery}
           </Text>
