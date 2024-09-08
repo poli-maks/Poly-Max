7205cc03f71cab875f1d302d32cfa730ffc1c540
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 16:11:21 +0300

    update url

--- app/[lang]/catalog/[...slug]/page.tsx
+++ app/[lang]/catalog/[...slug]/page.tsx
@@ -1,6 +1,4 @@
-// File: /app/[lang]/catalog/[...slug]/page.tsx
-
-import { getProductBySlug } from '@/app/lib/api/services'
+import { fetchProductBySlug } from '@/app/lib/api/services' // Corrected import name
 import { getDictionary } from '@/app/lib/dictionary'
 import { IParams } from '@/app/lib/interfaces'
 import Product from '@/app/ui/ProductPage/Product'
@@ -8,9 +6,9 @@ import SingleProductSkeleton from '@/app/ui/Skeletons/SingleProductSkeleton'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
-export const generateMetadata = async ({ params: { id: slug, lang } }: IParams) => { // Corrected parameter naming
+export const generateMetadata = async ({ params: { id: slug, lang } }: IParams) => { // Use fetchProductBySlug
 	let data
-	if (slug) data = await getProductBySlug(lang, slug)
+	if (slug) data = await fetchProductBySlug(lang, slug) // Corrected to use fetchProductBySlug
 
 	const { attributes: product } = data[0]
 
@@ -39,7 +37,7 @@ export const generateMetadata = async ({ params: { id: slug, lang } }: IParams) 
 	}
 }
 
-const ProductPage: React.FC<IParams> = async ({ params: { lang, id: slug } }) => { // Adjust parameter name to 'id: slug'
+const ProductPage: React.FC<IParams> = async ({ params: { lang, id: slug } }) => { // Use fetchProductBySlug
 	if (!slug) return notFound()
 
 	const dictionary = await getDictionary(lang)
