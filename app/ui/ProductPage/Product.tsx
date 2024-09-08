c90741af52985faad1188c47f11f294899d660be
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 23:42:26 +0300

    update

--- app/ui/ProductPage/Product.tsx
+++ app/ui/ProductPage/Product.tsx
@@ -1,4 +1,4 @@
-import { fetchProductBySlug } from '@/app/lib/api/services' // Updated to use fetchProductBySlug
+import { fetchProductBySlug } from '@/app/lib/api/services'
 import { IDictionaryModal, IProductDictionary } from '@/app/lib/interfaces'
 import { Locale } from '@/i18n.config'
 import { Flex } from '@chakra-ui/react'
@@ -23,21 +23,14 @@ interface IProps {
 
 const Product = async ({ dictionary, dictionaryModal, lang, slug }: IProps) => {
 	try {
-		// Debugging log
-		console.log(`Fetching product with slug: ${slug} for lang: ${lang}`);
-		
-		const product = await fetchProductBySlug(lang, slug); // Fetch product by slug
+		const product = await fetchProductBySlug(lang, slug) // Fetch product by slug
 		if (!product) {
-			console.error('No product data returned for slug:', slug);
-			return <div>Error: No product found.</div>;
+			return <div>Error: No product found.</div>
 		}
 
-		// Log the entire product data to check its structure
-		console.log('Fetched product data:', product);
-
-		const productImages = product[0]?.attributes?.img?.data || [];
+		const productImages = product[0]?.attributes?.img?.data || []
 		if (!productImages.length) {
-			console.error('No product images found for slug:', slug);
+			return <div>Error: No product images found.</div>
 		}
 
 		return (
@@ -54,11 +47,10 @@ const Product = async ({ dictionary, dictionaryModal, lang, slug }: IProps) => {
 					/>
 				</Flex>
 			</SectionWrapper>
-		);
+		)
 	} catch (error) {
-		console.error('Error in Product component:', error);
-		return <div>Application error: Please check logs for more details.</div>;
+		return <div>Application error: Please check logs for more details.</div>
 	}
-};
+}
 
-export default Product;
+export default Product
