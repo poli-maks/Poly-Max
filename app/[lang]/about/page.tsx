f2672326d43ca585569c4b0a8f2beec38c527df8
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 20:32:11 +0300

    update

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,6 +1,7 @@
 import { getDictionary } from '@/app/lib/dictionary'
 import { IParams } from '@/app/lib/interfaces'
 import About from '@/app/ui/AboutPage/About'
+import { Locale } from '@/i18n.config'
 
 export const metadata = {
 	title: 'About',
@@ -14,7 +15,9 @@ export const metadata = {
 }
 
 const AboutPage: React.FC<IParams> = async ({ params: { lang } }) => {
-	const dictionary = await getDictionary(lang)
+	// Cast lang to the specific Locale type
+	const locale = lang as Locale; // Ensure lang is of type 'de' | 'en'
+	const dictionary = await getDictionary(locale)
 
 	return <About dictionary={dictionary.aboutUsPage} />
 }
