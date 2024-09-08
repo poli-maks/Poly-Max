18fab53d7bc046d944fcd4ce55c9165cdef4e62b
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 23:52:49 +0300

    update

--- app/ui/ProductPage/Product.tsx
+++ app/ui/ProductPage/Product.tsx
@@ -24,21 +24,26 @@ interface IProps {
 const Product = async ({ dictionary, dictionaryModal, lang, slug }: IProps) => {
 	try {
 		// Fetch product data by slug
+		console.log(`Fetching product data for slug: ${slug}, lang: ${lang}`) // Log the request details
 		const product = await fetchProductBySlug(lang, slug)
+
+		// Log the product response data to check its structure
+		console.log('Product data fetched:', product)
 
 		// Check if product data exists
 		if (!product || product.length === 0) {
 			console.error(`No product found for slug: ${slug}`)
-
 			return <div>Error: No product found.</div>
 		}
 
 		const productImages = product[0]?.attributes?.img?.data || []
 
+		// Log the product images to verify they exist
+		console.log('Product images:', productImages)
+
 		// Check if images exist
 		if (!productImages.length) {
 			console.error(`No product images found for slug: ${slug}`)
-
 			return <div>Error: No product images found.</div>
 		}
 
@@ -59,8 +64,8 @@ const Product = async ({ dictionary, dictionaryModal, lang, slug }: IProps) => {
 			</SectionWrapper>
 		)
 	} catch (error) {
+		// Log detailed error message
 		console.error('Error fetching product data:', error)
-
 		return <div>Application error: Please check logs for more details.</div>
 	}
 }
