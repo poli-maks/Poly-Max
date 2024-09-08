febb736e8475dc6764b44010a69e68651711a755
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 14:45:10 +0300

    url by slug

--- app/[lang]/catalog/[...id]/page.tsx
+++ app/[lang]/catalog/[...id]/page.tsx
@@ -7,54 +7,55 @@ import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
 export const generateMetadata = async ({ params: { id, lang } }: IParams) => {
-  let data
-  if (id) data = await getProductBySlug(lang, parseInt(id))
+	let data
+	// No need to parse id to an integer, pass it directly as a string
+	if (id) data = await getProductBySlug(lang, id)
 
-  const { attributes: product } = data[0]
+	const { attributes: product } = data[0]
 
-  const imgUrl =
-    product.img.data !== null
-      ? product.img.data[0].attributes.formats?.small?.url
-      : '/img/productPlaceholder.jpg'
+	const imgUrl =
+		product.img.data !== null
+			? product.img.data[0].attributes.formats?.small?.url
+			: '/img/productPlaceholder.jpg'
 
-  return {
-    title: product.title,
-    alternates: {
-      canonical: `/catalog/${id}`,
-      languages: {
-        en: `/en/catalog/${id}`,
-        de: `/de/catalog/${id}`,
-      },
-    },
-    description: product.descShort,
-    openGraph: {
-      images: [
-        {
-          url: imgUrl,
-        },
-      ],
-    },
-  }
+	return {
+		title: product.title,
+		alternates: {
+			canonical: `/catalog/${id}`,
+			languages: {
+				en: `/en/catalog/${id}`,
+				de: `/de/catalog/${id}`,
+			},
+		},
+		description: product.descShort,
+		openGraph: {
+			images: [
+				{
+					url: imgUrl,
+				},
+			],
+		},
+	}
 }
 
 const ProductPage: React.FC<IParams> = async ({ params: { lang, id } }) => {
-  if (!id) return notFound()
+	if (!id) return notFound()
 
-  const dictionary = await getDictionary(lang)
+	const dictionary = await getDictionary(lang)
 
-  return (
-    <>
-      <Suspense fallback={<SingleProductSkeleton />}>
-        <Product
-          lang={lang}
-          id={id}
-          //product={product}
-          dictionary={dictionary.productPage}
-          dictionaryModal={dictionary.modalForm}
-        />
-      </Suspense>
-    </>
-  )
+	return (
+		<>
+			<Suspense fallback={<SingleProductSkeleton />}>
+				<Product
+					lang={lang}
+					id={id}
+					//product={product}
+					dictionary={dictionary.productPage}
+					dictionaryModal={dictionary.modalForm}
+				/>
+			</Suspense>
+		</>
+	)
 }
 
 export default ProductPage
