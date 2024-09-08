490f330d581c34361729a7a9fb830421ad47d5e2
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 20:21:06 +0300

    update

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,22 +1,24 @@
-import { getDictionary } from '@/app/lib/dictionary'
-import { IParams } from '@/app/lib/interfaces'
-import About from '@/app/ui/AboutPage/About'
+import { getDictionary } from '@/app/lib/dictionary';
+import { IParams } from '@/app/lib/interfaces';
+import About from '@/app/ui/AboutPage/About';
+import { Locale } from '@/i18n.config'; // Import the Locale type
 
 export const metadata = {
-	title: 'About',
-	alternates: {
-		canonical: '/about',
-		languages: {
-			en: '/en/about',
-			de: '/de/about',
-		},
-	},
-}
+  title: 'About',
+  alternates: {
+    canonical: '/about',
+    languages: {
+      en: '/en/about',
+      de: '/de/about',
+    },
+  },
+};
 
-const AboutPage: React.FC<IParams> = async ({ params: { lang } }) => {
-	const dictionary = await getDictionary(lang)
+const AboutPage: React.FC<{ params: IParams['params'] }> = async ({ params: { lang } }) => {
+  const locale = lang as Locale; // Explicitly cast `lang` to `Locale`
+  const dictionary = await getDictionary(locale);
 
-	return <About dictionary={dictionary.aboutUsPage} />
-}
+  return <About dictionary={dictionary.aboutUsPage} />;
+};
 
-export default AboutPage
+export default AboutPage;
