fda668c8c34975d151106f3004e6965ce4255ac7
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 20:03:17 +0300

    update

--- app/[lang]/catalog/[...slug]/page.tsx
+++ app/[lang]/catalog/[...slug]/page.tsx
@@ -1,44 +1,45 @@
 import { fetchProductBySlug } from '@/app/lib/api/services'
 import { getDictionary } from '@/app/lib/dictionary'
-import { IProductDictionary, IDictionaryModal } from '@/app/lib/interfaces'
+import { IProductDictionary, IDictionaryModal, IParams } from '@/app/lib/interfaces'
 import { notFound } from 'next/navigation'
 import Product from '@/app/ui/ProductPage/Product'
 
-export const generateMetadata = async ({ params: { slug, lang } }) => {
-	// Your metadata generation logic (if any)
+export const generateMetadata = async ({ params }: { params: IParams }) => {
+    const { slug, lang } = params
+    // Your metadata generation logic (if any)
 }
 
-const ProductPage = async ({ params: { slug, lang } }) => {
-	try {
-		// Fetch the product by slug
-		const product = await fetchProductBySlug(lang, slug)
-		if (!product || product.length === 0) {
-			return notFound()
-		}
+const ProductPage = async ({ params }: { params: IParams }) => {
+    const { slug, lang } = params
+    try {
+        // Fetch the product by slug
+        const product = await fetchProductBySlug(lang, slug)
+        if (!product || product.length === 0) {
+            return notFound()
+        }
 
-		// Fetch the dictionary for the given language
-		const dictionary: IProductDictionary = await getDictionary(lang)
-		// Fetch the modal dictionary (if needed)
-		const dictionaryModal: IDictionaryModal = {/* Your dictionaryModal data */ }
+        // Fetch the dictionary for the given language
+        const dictionary: IProductDictionary = await getDictionary(lang)
+        // Fetch the modal dictionary (if needed)
+        const dictionaryModal: IDictionaryModal = {/* Your dictionaryModal data */}
 
-		// Render the Product component
-		return (
-			<>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Product
-						lang={lang}
-						id={product[0].id.toString()}
-						dictionary={dictionary}
-						slug={slug} // Pass the slug
-						dictionaryModal={dictionaryModal} // Pass the dictionaryModal
-					/>
-				</Suspense>
-			</>
-		)
-	} catch (error) {
-		console.error("Error in ProductPage:", error)
-		return notFound()
-	}
+        // Render the Product component
+        return (
+            <>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Product
+                        lang={lang}
+                        slug={slug}
+                        dictionary={dictionary}
+                        dictionaryModal={dictionaryModal}
+                    />
+                </Suspense>
+            </>
+        )
+    } catch (error) {
+        console.error("Error in ProductPage:", error)
+        return notFound()
+    }
 }
 
 export default ProductPage
