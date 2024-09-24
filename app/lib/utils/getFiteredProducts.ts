0a939d259158f47c5926ef54b077032db0b5cf16
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 12:41:39 +0300

    update

--- app/lib/utils/getFiteredProducts.ts
+++ app/lib/utils/getFiteredProducts.ts
@@ -1,30 +1,34 @@
 import { Locale } from '@/i18n.config'
 
 import {
-	fetchAllProducts,
-	fetchProductsByCategory,
-	fetchProductsBySubCategory,
-	searchProductsByTitle,
+  fetchAllProducts,
+  fetchProductsByCategory,
+  fetchProductsBySubCategory,
+  searchProductsByTitle,
 } from '../api/services'
 import { SEARCH_PARAMS } from '../interfaces'
 
-export const getFiteredProducts = async (
-	lang: Locale,
-	params: { [key in SEARCH_PARAMS]: string }
+export const getFilteredProducts = async (
+  lang: Locale,
+  params: { [key in SEARCH_PARAMS]: string }
 ) => {
-	const { category, page, sub, search, query } = params
-	if (category) {
-		const data = sub
-			? await fetchProductsBySubCategory(lang, parseInt(sub), parseInt(page))
-			: await fetchProductsByCategory(lang, category, parseInt(page))
+  const { category, page, sub, search, query } = params
 
-		return data
-	}
-	if (search && query) {
-		const data = await searchProductsByTitle(lang, query, parseInt(page))
+  // If a category is provided, check for sub-category and fetch accordingly
+  if (category) {
+    const data = sub
+      ? await fetchProductsBySubCategory(lang, parseInt(sub), parseInt(page))
+      : await fetchProductsByCategory(lang, category, parseInt(page))
 
-		return data
-	}
+    return data
+  }
 
-	return await fetchAllProducts(lang, parseInt(page))
+  // If a search query is provided, fetch products by title
+  if (search && query) {
+    const data = await searchProductsByTitle(lang, query, parseInt(page))
+    return data
+  }
+
+  // Default to fetching all products if no filters are applied
+  return await fetchAllProducts(lang, parseInt(page))
 }
