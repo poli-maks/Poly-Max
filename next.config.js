e250eb8a4af25cee5f5833320455476d61c62017
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 17:12:45 +0300

    update

--- next.config.js
+++ next.config.js
@@ -12,17 +12,6 @@ const nextConfig = {
 	experimental: {
 		missingSuspenseWithCSRBailout: false,
 	},
-	async redirects() {
-    return [
-      {
-        source: '/en/catalog/4)', // Match only numeric IDs
-        destination: '/en/catalog/4-barrage-post', // Redirect to the URL with the slug
-        permanent: true, // This makes the redirect permanent (301)
-      },
-      
-      // Add more redirects here manually for each product
-    ];
-  },
 }
 
 module.exports = nextConfig
