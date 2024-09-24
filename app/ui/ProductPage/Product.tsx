d8a81b129b00fd47e89523426dd1fdf877e1b308
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 12:29:01 +0300

    updt

--- app/ui/ProductPage/Product.tsx
+++ app/ui/ProductPage/Product.tsx
@@ -1,46 +1,52 @@
-import { fetchProductByUid } from '@/app/lib/api/services'
-import { IDictionaryModal, IProductDictionary } from '@/app/lib/interfaces'
-import { Locale } from '@/i18n.config'
-import { Flex } from '@chakra-ui/react'
-import dynamic from 'next/dynamic'
+import { getProductByUid } from '@/app/lib/api/services';
+import { IDictionaryModal, IProductDictionary } from '@/app/lib/interfaces';
+import { Locale } from '@/i18n.config';
+import { Flex } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+import SectionWrapper from '../sectionWrapper/SectionWrapper';
+import { ProductContent } from './productContent/ProductContent';
+import { notFound } from 'next/navigation';
 
-import SectionWrapper from '../sectionWrapper/SectionWrapper'
-import { ProductContent } from './productContent/ProductContent'
-
-const ImageSection = dynamic(
-	() => {
-		return import('./productSlider/ImagesSection')
-	},
-	{ ssr: false }
-)
+const ImageSection = dynamic(() => import('./productSlider/ImagesSection'), { ssr: false });
 
 interface IProps {
-	//product: IProductProps[]
-	id: string
-	lang: Locale
-	dictionary: IProductDictionary
-	dictionaryModal: IDictionaryModal
+  id: string;
+  lang: Locale;
+  dictionary: IProductDictionary;
+  dictionaryModal: IDictionaryModal;
 }
 
 const Product = async ({ dictionary, dictionaryModal, lang, id }: IProps) => {
-	const product = await fetchProductByUid(lang, parseInt(id))
-	const productImages = product[0].attributes.img.data
+  // Fetch the product data by UID
+  const product = await getProductByUid(lang, parseInt(id));
 
-	return (
-		<SectionWrapper>
-			<Flex flexDirection={{ base: 'column', lg: 'row' }}>
-				<Flex w={{ base: '100%', xl: '530px', lg: '330px' }}>
-					<ImageSection productImages={productImages} />
-				</Flex>
+  // Handle case if product is not found
+  if (!product || product.length === 0) {
+    return notFound();
+  }
 
-				<ProductContent
-					product={product}
-					dictionary={dictionary}
-					dictionaryModal={dictionaryModal}
-				/>
-			</Flex>
-		</SectionWrapper>
-	)
-}
+  const productAttributes = product[0]?.attributes;
+  const productImages = productAttributes?.img?.data;
 
-export default Product
+  return (
+    <SectionWrapper>
+      <Flex flexDirection={{ base: 'column', lg: 'row' }}>
+        <Flex w={{ base: '100%', xl: '530px', lg: '330px' }}>
+          {productImages ? (
+            <ImageSection productImages={productImages} />
+          ) : (
+            <p>No images available for this product.</p>
+          )}
+        </Flex>
+
+        <ProductContent
+          product={productAttributes}
+          dictionary={dictionary}
+          dictionaryModal={dictionaryModal}
+        />
+      </Flex>
+    </SectionWrapper>
+  );
+};
+
+export default Product;
