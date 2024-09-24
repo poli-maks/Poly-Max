2d5909f23eca0c175daeb1643e5d47a1dcbba00c
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 12:34:30 +0300

    updt

--- app/ui/ProductPage/Product.tsx
+++ app/ui/ProductPage/Product.tsx
@@ -1,32 +1,36 @@
-import { getProductByUid } from '@/app/lib/api/services';
-import { IDictionaryModal, IProductDictionary } from '@/app/lib/interfaces';
-import { Locale } from '@/i18n.config';
-import { Flex } from '@chakra-ui/react';
-import dynamic from 'next/dynamic';
-import SectionWrapper from '../sectionWrapper/SectionWrapper';
-import { ProductContent } from './productContent/ProductContent';
-import { notFound } from 'next/navigation';
+import { fetchProductByUid } from '@/app/lib/api/services'
+import { IDictionaryModal, IProductDictionary } from '@/app/lib/interfaces'
+import { Locale } from '@/i18n.config'
+import { Flex } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
 
-const ImageSection = dynamic(() => import('./productSlider/ImagesSection'), { ssr: false });
+import SectionWrapper from '../sectionWrapper/SectionWrapper'
+import { ProductContent } from './productContent/ProductContent'
+import { notFound } from 'next/navigation'
+
+const ImageSection = dynamic(
+  () => import('./productSlider/ImagesSection'),
+  { ssr: false }
+)
 
 interface IProps {
-  id: string;
-  lang: Locale;
-  dictionary: IProductDictionary;
-  dictionaryModal: IDictionaryModal;
+  id: string
+  lang: Locale
+  dictionary: IProductDictionary
+  dictionaryModal: IDictionaryModal
 }
 
 const Product = async ({ dictionary, dictionaryModal, lang, id }: IProps) => {
   // Fetch the product data by UID
-  const product = await getProductByUid(lang, parseInt(id));
+  const product = await fetchProductByUid(lang, parseInt(id))
 
   // Handle case if product is not found
   if (!product || product.length === 0) {
-    return notFound();
+    return notFound()
   }
 
-  const productAttributes = product[0]?.attributes;
-  const productImages = productAttributes?.img?.data;
+  const productAttributes = product[0]?.attributes
+  const productImages = productAttributes?.img?.data
 
   return (
     <SectionWrapper>
@@ -46,7 +50,7 @@ const Product = async ({ dictionary, dictionaryModal, lang, id }: IProps) => {
         />
       </Flex>
     </SectionWrapper>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
