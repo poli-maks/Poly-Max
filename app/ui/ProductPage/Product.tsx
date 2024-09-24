cb348d6a7021c5d95503a51ce7fc28a322589fe7
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 18:29:04 +0300

    update

--- app/ui/ProductPage/Product.tsx
+++ app/ui/ProductPage/Product.tsx
@@ -20,6 +20,7 @@ interface IProps {
 	lang: Locale
 	dictionary: IProductDictionary
 	dictionaryModal: IDictionaryModal
+	product: any
 }
 
 const Product = async ({ dictionary, dictionaryModal, lang, id }: IProps) => {
