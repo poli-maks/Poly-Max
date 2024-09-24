80155d8b7bb03285cfe4e57841b87941a107cb30
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 23:54:42 +0300

    1

--- app/ui/CatalogPage/ProductItem/ProductItem.tsx
+++ app/ui/CatalogPage/ProductItem/ProductItem.tsx
@@ -3,70 +3,80 @@ import { Box, Heading } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const ProductItem = ({ product: { attributes }, lang }: { product: IProduct; lang: string }) => {
-	return (
-		<Box
-			as={'li'}
-			key={attributes.uid}
-			position={'relative'}
-			css={{
-				'&:hover .product_card': {
-					transform: 'scale(1.05)',
-					transition: 'all 300ms ease-in-out',
-				},
-			}}
-		>
-			<Link href={`/${lang}/catalog/${attributes.uid}`}>
-				<article>
-					<Box
-						position="relative"
-						z-index="2"
-						width="100%"
-						height="300px"
-						overflow={'hidden'}
-						_hover={{
-							cursor: 'pointer',
-						}}
-						backgroundImage={
-							attributes.img.data
-								? `url('${
-										attributes.img && attributes.img?.data[0].attributes.formats?.thumbnail.url
-									}')`
-								: '/img/blurPlaceholder.png'
-						}
-						backgroundPosition={'center'}
-						backgroundRepeat={'no-repeat'}
-						backgroundSize={'cover'}
-					>
-						<Image
-							className="product_card"
-							src={
-								(attributes.img.data && attributes.img?.data[0].attributes.url) ||
-								'/img/productPlaceholder.jpg'
-							}
-							alt={attributes.title + '' + attributes.descShort || 'product image'}
-							fill
-							sizes="(max-width: 768px) 100vw, (max-width: 1300px) 50vw, 33vw"
-							style={{ objectFit: 'cover', transition: 'all 300ms ease-in-out' }}
-							placeholder="blur"
-							blurDataURL="/img/blurPlaceholder.png"
-							loading="lazy"
-						/>
-					</Box>
+// Функція для створення slug з тайтлу товару
+const generateSlugFromTitle = (title: string): string => {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-') // Заміна всіх символів, що не є буквами або цифрами, на дефіси
+    .replace(/(^-|-$)+/g, '') // Видалення початкових і кінцевих дефісів
+}
 
-					<Heading
-						color={'#000'}
-						fontFamily={'inherit'}
-						fontSize={'14'}
-						mt={'10px'}
-						fontWeight={'500'}
-					>
-						{attributes?.title || ''}
-					</Heading>
-				</article>
-			</Link>
-		</Box>
-	)
+const ProductItem = ({ product: { attributes }, lang }: { product: IProduct; lang: string }) => {
+  // Генеруємо slug з тайтлу товару
+  const slug = generateSlugFromTitle(attributes.title);
+
+  return (
+    <Box
+      as={'li'}
+      key={attributes.uid}
+      position={'relative'}
+      css={{
+        '&:hover .product_card': {
+          transform: 'scale(1.05)',
+          transition: 'all 300ms ease-in-out',
+        },
+      }}
+    >
+      {/* Додаємо slug до URL товару */}
+      <Link href={`/${lang}/catalog/${attributes.uid}-${slug}`}>
+        <article>
+          <Box
+            position="relative"
+            z-index="2"
+            width="100%"
+            height="300px"
+            overflow={'hidden'}
+            _hover={{
+              cursor: 'pointer',
+            }}
+            backgroundImage={
+              attributes.img.data
+                ? `url('${attributes.img && attributes.img?.data[0].attributes.formats?.thumbnail.url}')`
+                : '/img/blurPlaceholder.png'
+            }
+            backgroundPosition={'center'}
+            backgroundRepeat={'no-repeat'}
+            backgroundSize={'cover'}
+          >
+            <Image
+              className="product_card"
+              src={
+                (attributes.img.data && attributes.img?.data[0].attributes.url) ||
+                '/img/productPlaceholder.jpg'
+              }
+              alt={attributes.title + '' + attributes.descShort || 'product image'}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1300px) 50vw, 33vw"
+              style={{ objectFit: 'cover', transition: 'all 300ms ease-in-out' }}
+              placeholder="blur"
+              blurDataURL="/img/blurPlaceholder.png"
+              loading="lazy"
+            />
+          </Box>
+
+          <Heading
+            color={'#000'}
+            fontFamily={'inherit'}
+            fontSize={'14'}
+            mt={'10px'}
+            fontWeight={'500'}
+          >
+            {attributes?.title || ''}
+          </Heading>
+        </article>
+      </Link>
+    </Box>
+  )
 }
 
 export default ProductItem
