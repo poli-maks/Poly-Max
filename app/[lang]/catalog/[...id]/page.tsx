636750ff5f21e7133332224996cd390fa8688d36
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 13:23:25 +0300

    url

--- app/[lang]/catalog/[...id]/page.tsx
+++ app/[lang]/catalog/[...id]/page.tsx
@@ -6,55 +6,68 @@ import SingleProductSkeleton from '@/app/ui/Skeletons/SingleProductSkeleton'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
+// Helper function to extract ID from the slug
+const extractIdFromSlug = (idSlug: string) => {
+  const [id] = idSlug.split('-')
+  return parseInt(id, 10)
+}
+
 export const generateMetadata = async ({ params: { id, lang } }: IParams) => {
-	let data
-	if (id) data = await getProductByUid(lang, parseInt(id))
+  const productId = extractIdFromSlug(id)
 
-	const { attributes: product } = data[0]
+  let data
+  if (productId) data = await getProductByUid(lang, productId)
 
-	const imgUrl =
-		product.img.data !== null
-			? product.img.data[0].attributes.formats?.small?.url
-			: '/img/productPlaceholder.jpg'
+  if (!data || data.length === 0) return notFound()
 
-	return {
-		title: product.title,
-		alternates: {
-			canonical: `/catalog/${id}`,
-			languages: {
-				en: `/en/catalog/${id}`,
-				de: `/de/catalog/${id}`,
-			},
-		},
-		description: product.descShort,
-		openGraph: {
-			images: [
-				{
-					url: imgUrl,
-				},
-			],
-		},
-	}
+  const { attributes: product } = data[0]
+
+  const imgUrl =
+    product.img?.data !== null
+      ? product.img.data[0]?.attributes?.formats?.small?.url
+      : '/img/productPlaceholder.jpg'
+
+  return {
+    title: product.title,
+    alternates: {
+      canonical: `/catalog/${id}`,
+      languages: {
+        en: `/en/catalog/${id}`,
+        de: `/de/catalog/${id}`,
+      },
+    },
+    description: product.descShort,
+    openGraph: {
+      images: [
+        {
+          url: imgUrl,
+        },
+      ],
+    },
+  }
 }
 
 const ProductPage: React.FC<IParams> = async ({ params: { lang, id } }) => {
-	if (!id) return notFound()
+  if (!id) return notFound()
 
-	const dictionary = await getDictionary(lang)
+  const productId = extractIdFromSlug(id)
 
-	return (
-		<>
-			<Suspense fallback={<SingleProductSkeleton />}>
-				<Product
-					lang={lang}
-					id={id}
-					//product={product}
-					dictionary={dictionary.productPage}
-					dictionaryModal={dictionary.modalForm}
-				/>
-			</Suspense>
-		</>
-	)
+  if (!productId) return notFound()
+
+  const dictionary = await getDictionary(lang)
+
+  return (
+    <>
+      <Suspense fallback={<SingleProductSkeleton />}>
+        <Product
+          lang={lang}
+          id={productId.toString()}
+          dictionary={dictionary.productPage}
+          dictionaryModal={dictionary.modalForm}
+        />
+      </Suspense>
+    </>
+  )
 }
 
 export default ProductPage
