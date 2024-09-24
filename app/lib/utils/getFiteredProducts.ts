d2b4ed67c73ab35d4f1c82134781de758e6f3c48
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-24 12:43:42 +0300

    updt

--- app/lib/utils/getFiteredProducts.ts
+++ app/lib/utils/getFiteredProducts.ts
@@ -1,10 +1,10 @@
 import { Locale } from '@/i18n.config'
 
 import {
-  fetchAllProducts,
-  fetchProductsByCategory,
-  fetchProductsBySubCategory,
-  searchProductsByTitle,
+  getAllProducts, // Change this from 'fetchAllProducts' to 'getAllProducts'
+  getProductsByCategory, // Change from 'fetchProductsByCategory'
+  getProductsBySubCategory, // Change from 'fetchProductsBySubCategory'
+  searchProductsByTitle // Ensure this is correctly exported in services.ts
 } from '../api/services'
 import { SEARCH_PARAMS } from '../interfaces'
 
@@ -14,21 +14,18 @@ export const getFilteredProducts = async (
 ) => {
   const { category, page, sub, search, query } = params
 
-  // If a category is provided, check for sub-category and fetch accordingly
   if (category) {
     const data = sub
-      ? await fetchProductsBySubCategory(lang, parseInt(sub), parseInt(page))
-      : await fetchProductsByCategory(lang, category, parseInt(page))
+      ? await getProductsBySubCategory(lang, parseInt(sub), parseInt(page)) // Updated function name
+      : await getProductsByCategory(lang, category, parseInt(page)) // Updated function name
 
     return data
   }
 
-  // If a search query is provided, fetch products by title
   if (search && query) {
     const data = await searchProductsByTitle(lang, query, parseInt(page))
     return data
   }
 
-  // Default to fetching all products if no filters are applied
-  return await fetchAllProducts(lang, parseInt(page))
+  return await getAllProducts(lang, parseInt(page)) // Updated function name
 }
